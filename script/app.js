ed1997766c1d3be0e800060715d106ee60dbacf2
Igor_Alexandrovich <i_swat@mail.ru>
2021-07-06 18:29:20 +0300

    Урок 3. Асинхронные запросы

--- script/app.js
+++ script/app.js
@@ -1,280 +1,205 @@
-class GoodsItem {
-	constructor(id, title , price, image='not-img.png') {
-		this.id = id;
-		this.title = title;
-		this.price = price;
-		this.image = image;
-	}
+function makeGETRequest(url, callback) {
+    return new Promise((resolve, reject) => {
+        let xhr;
 
-	render() {
-		return `
-			<div class='goods-item'>
-				<div class='goods-imgWrap'>
-					<img class='goods-img' src='images/featured/${this.image}' alt='${this.title}'>
-					<div class='goods-bgDark'></div>
-				</div>
-		
-				<div class='goods-cardBottom'>
-						<h3 class='goods-title'>${this.title}</h3>
-						<p class='goods-price'>$<span>${this.price}</span></p>
-						<button class='goods-btn' data-productId='${this.id}'>Добавить</button>
-				</div>
-				
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                callback(xhr.responseText);
+            }
+        }
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+}
+
+class GoodsItem {
+    constructor(id_product, product_name, price, image = 'not-img.png') {
+        this.id_product = id_product;
+        this.product_name = product_name;
+        this.price = price;
+        this.image = image;
+    }
+
+    render() {
+        return `
+		<div class='goods-item'>
+			<div class='goods-imgWrap'>
+				<img class='goods-img' src='images/featured/${this.image}' alt='${this.product_name}'>
 			</div>
+
+			<div class='goods-cardBottom'>
+				<h3 class='goods-title'>${this.product_name}</h3>
+				<p class='goods-price'>$<span>${this.price}</span></p>
+				<button class='goods-btn' data-productId='${this.id_product}'>Добавить</button>
+			</div>
+		</div>
 	`;
-	}
+    }
 }
 
 class GoodsList {
-	constructor() {
-		this.goods = [];
-	}
-	fetchGoods() {
-		this.goods = [
-			{ id: 0, title: '1.Men\'s jacket', price: 135.22, image: '0.jpg' },
-			{ id: 1, title: '2.Women\'s costume', price: 141.99, image: '1.jpg' },
-			{ id: 2, title: '3.Women\'s shirt', price: 152.45, image: '2.jpg' },
-			{ id: 3, title: '4.Men\'s sweater', price: 145.33, image: '3.jpg' },
-			{ id: 4, title: '5.Women\'s jeans', price: 137.55, image: '4.jpg' },
-			{ id: 5, title: '6.Women\'s jacket', price: 142.99, image: '5.jpg' },
-			{ id: 6, title: '7.Women\'s jacket', price: 145.99},
-			{ id: 7, title: '8.Women\'s jacket', price: 149.99},
-			{ id: 8, title: '9.Women\'s jacket', price: 139.99},
-		];
-	}
-	render() {
-		let listHtml = '';
-		this.goods.forEach(good => {
-			const goodItem = new GoodsItem(good.id, good.title, good.price, good.image);
-			listHtml += goodItem.render();
-		});
-		document.querySelector('.goods-list').innerHTML = listHtml;
-	}
+    constructor() {
+        this.goods = [];
+    }
 
-	sumAllProducts(){
-		return this.goods.reduce((sum, good) => sum += good.price, 0);
-	}
+    fetchGoods(url) {
+        makeGETRequest(url, (goods) => {
+            this.goods = JSON.parse(goods);
+            this.render();
+            // this.sumAllProducts();
+        })
+    }
+
+    render() {
+        let listHtml = '';
+        console.log("render = ", this.goods);
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id_product, good.product_name, good.price, good.image);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+    sumAllProducts() {
+        return this.goods.reduce((sum, good) => {
+            sum += good.price
+        }, 0);
+    }
 }
-
-const list = new GoodsList();
-list.fetchGoods();
-list.render();
-console.log('Сумма все продуктов = ' + list.sumAllProducts());
 
 class Basket {
-	constructor() {}
+    constructor() {
+        document.querySelector('.cart-iconWrap').insertAdjacentHTML('afterend', this.renderBasket());
+        this.basketGoods = {};
+    }
 
-	/**
-	 * Добавляет продукт
-	 */
-	addProductBasket() {}
+    //Функция добавляет продукт в объект basketGoods.
+    addProductToObject(productId) {
+        console.log('addProductToObject = ' + productId);
 
-	/**
-	 * Обновление содержимого корзины
-	 */
-	updateProductBasket(){}
+        if (!(productId in this.basketGoods)) {
+            this.basketGoods[productId] = 1;
+        } else {
+            this.basketGoods[productId]++;
+        }
+    }
 
-	/**
-	 * Отрисовывание продукт в корзине.
-	 */
-	render() {}
+    //Первая отрисовка корзины
+    renderBasket() {
+        return `<div class='basket hidden'>
+                    <div class='basket-row basket-header'>
+                    <div>Название товара</div>
+                    <div>Количество</div>
+                    <div>Цена за шт.</div>
+                    <div>Итого</div>
+                </div>
+
+                <div class='basket-total'>
+                    Товаров в корзине на сумму: $<span class='basket-total-value'>0</span>
+                </div>
+        </div>`;
+    }
+
+    //Функция отрисовывает новый товар в корзине.
+    renderNewProductInBasket(productId) {
+        let productRow = `
+        <div class="basket-row">
+            <div>${this.basketGoods[productId].product_name}</div>
+            <div>
+                <span class="product-count" data-productId="${productId}">1</span> шт.
+            </div>
+            <div>$${this.basketGoods[productId].price}</div>
+            <div>
+                $<span class="product-totalRow" data-productId="${productId}">${this.basketGoods[productId].price}</span>
+            </div>
+        </div>
+    `;
+        document.querySelector('.basket-total').insertAdjacentHTML("beforebegin", productRow);
+    }
+
+    //Функция срабатывает когда нужно отрисовать продукт в корзине.
+    renderProductInBasket(productId) {
+        let productExist = document.querySelector(`.product-count[data-productId="${productId}"]`);
+        if (productExist) {
+            this.increaseProductCount(productId);
+            this.recalculateSumForProduct(productId);
+        } else {
+            this.renderNewProductInBasket(productId);
+        }
+    }
+
+    //Функция увеличивает счетчик количества товаров рядом с иконкой корзины.
+    increaseProductsCount() {
+        document.querySelector('.cart-iconWrap span').textContent++;
+    }
+
+    //Функция увеличивает количество товаров в строке в корзине.
+    increaseProductCount(productId) {
+        const productCountEl = document.querySelector(`.product-count[data-productId="${productId}"]`);
+        productCountEl.textContent++;
+    }
+
+    //Функция пересчитывает стоимость товара умноженное на количество товара в корзине.
+    recalculateSumForProduct(productId) {
+        const productTotalRowEl = document.querySelector(`.product-totalRow[data-productId="${productId}"]`);
+        let totalPriceForRow = (this.basketGoods[productId] * this.basketGoods[productId].price).toFixed(2);
+        productTotalRowEl.textContent = totalPriceForRow;
+    }
+
+    //Функция пересчитывает общую стоимость корзины и выводит это значение на страницу.
+    calculateAndRenderTotalBasketSum() {
+        let totalSum = 0;
+        for (let productId in this.basketGoods) {
+            totalSum += this.basketGoods[productId] * this.basketGoods[productId].price;
+        }
+        document.querySelector('.basket-total-value').textContent = totalSum.toFixed(2);
+    }
+
+    // Функция назначает обработку клика на все кнопки "Добавить".
+    addEventListenersForAddToCartButtons() {
+        const addToCartBtns = document.querySelectorAll('button[data-productId]');
+        addToCartBtns.forEach(function (button) {
+            button.addEventListener('click', (event) => {
+                const productId = event.currentTarget.getAttribute('data-productId');
+                this.run(productId);
+                console.log('click');
+            });
+        })
+    }
+
+    //Запуск корзины
+    run(productId) {
+        this.increaseProductsCount();
+        this.addProductToObject(productId);
+        this.renderProductInBasket(productId);
+        this.calculateAndRenderTotalBasketSum();
+    }
+
+    //Функция назначает обработку клика на все кнопки "Добавить".
+    addEventListenersForAddToCartButtons() {
+        return new Promise((resolve, reject) => {
+            const addToCartBtns = document.querySelectorAll('button[data-productId]');
+            addToCartBtns.forEach(function (button) {
+                button.addEventListener('click', function (event) {
+                    const productId = event.currentTarget.getAttribute('data-productId');
+                    this.run(productId);
+                });
+            })
+        })
+    }
 }
 
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
+const list = new GoodsList();
+list.fetchGoods(API_URL);
 
+const basket = new Basket();
+basket.addEventListenersForAddToCartButtons();
 
-
-
-
-
-
-
-
-
-
-
-
-// const pathImages = 'images';
-// const pathFeatured = `${pathImages}/featured`;
-// const featuredItemsEl = document.querySelector('.goods-list');
-
-// /**
-//  * Эта функция принимает один из объектов из массива products в файле productsArray.js.
-//  * @param {ProductDTO} product объект с информацией о продукте.
-//  * @returns {string} html-разметка карточки товара.
-//  */
-// function getProduct(product) {
-// 	return `
-// 	<div class='goods-item'>
-// 		<div class='goods-imgWrap'>
-// 			<img class='goods-img' src='${pathFeatured}/${product.image}' alt='${product.title}'>
-// 			<div class='goods-bgDark'></div>
-// 		</div>
-//
-// 		<div class='goods-cardBottom'>
-// 				<h3 class='goods-title'>${product.title}</h3>
-// 				<p class='goods-price'>$<span>${product.price}</span></p>
-// 				<button class='goods-btn' data-productId='${product.id}'>Добавить</button>
-// 		</div>
-//
-// 	</div>
-// 	`;
-// }
-
-// /**
-//  * Функция вставляет карточки товаров в страницу.
-//  * @param {ProductDTO[]} products массив товаров из файла productsArray.js
-//  * @param {HTMLDivElement} featuredItemsEl элемент с классом .goods-list
-//  */
-// function insertProductsIntoPage(products, featuredItemsEl) {
-// 	let productsMarkup = '';
-// 	for (let product of products) {
-// 		productsMarkup += getProduct(product);
-// 	}
-// 	featuredItemsEl.insertAdjacentHTML('afterbegin', productsMarkup);
-// }
-
-// /**
-//  * Функция назначает обработку клика на все кнопки "Добавить".
-//  */
-// function addEventListenersForAddToCartButtons() {
-// 	const addToCartBtns = document.querySelectorAll('button[data-productId]');
-// 	addToCartBtns.forEach(function (button) {
-// 		button.addEventListener('click', addedProductHandler);
-// 	})
-// }
-
-// /**
-//  * Функция-обработчик события клика по кнопке "Добавить".
-//  * @param {MouseEvent} event
-//  */
-// function addedProductHandler(event) {
-// 	const productId = event.currentTarget.getAttribute('data-productId');
-// 	addProductIntoBasket(productId);
-// }
-
-// insertProductsIntoPage(products, featuredItemsEl);
-// addEventListenersForAddToCartButtons();
-
-
-
-
-// const openBasketBtn = document.querySelector('.cart-iconWrap');
-// const basketEl = document.querySelector('.basket');
-// const basketCounterEl = document.querySelector('.cart-iconWrap span');
-// const basketTotalEl = document.querySelector('.basket-total');
-// const basketTotalValueEl = document.querySelector('.basket-total-value');
-
-// /**
-//  * Появление, скрытие окна списка товаров в корзине
-//  */
-// openBasketBtn.addEventListener('click', function () {
-// 	console.log('click' + basketEl);
-// 	basketEl.classList.toggle('hidden');
-// });
-
-// /**
-//  * В корзине хранится количество каждого товара
-//  * Ключ это id продукта, значение это количество товаров в корзине, например:
-//  {
-//     1: 10,
-//     3: 2
-//  }
-//  */
-// let basket = {};
-
-// /**basket
-//  * Функция добавляет продукт в объект basket.
-//  * @param {number} productId
-//  */
-// function addProductToObject(productId) {
-// 	if (!(productId in basket)) {
-// 		basket[productId] = 1;
-// 	} else {
-// 		basket[productId]++;
-// 	}
-// }
-
-// /**
-//  * Функция срабатывает когда нужно отрисовать продукт в корзине.
-//  * @param {number} productId
-//  */
-// function renderProductInBasket(productId) {
-// 	let productExist = document.querySelector(`.product-count[data-productId="${productId}"]`);
-// 	if (productExist) {
-// 		increaseProductCount(productId);
-// 		recalculateSumForProduct(productId);
-// 	} else {
-// 		renderNewProductInBasket(productId);
-// 	}
-// }
-//
-// /**
-//  * Функция отрисовывает новый товар в корзине.
-//  * @param {number} productId
-//  */
-// function renderNewProductInBasket(productId) {
-// 	// console.log('productId = ' + productId);
-//
-// 	let productRow = `
-//         <div class="basket-row">
-//             <div>${products[productId].title}</div>
-//             <div>
-//                 <span class="product-count" data-productId="${productId}">1</span> шт.
-//             </div>
-//             <div>$${products[productId].price}</div>
-//             <div>
-//                 $<span class="product-totalRow" data-productId="${productId}">${products[productId].price}</span>
-//             </div>
-//         </div>
-//     `;
-// 	basketTotalEl.insertAdjacentHTML("beforebegin", productRow);
-// }
-//
-// /**
-//  * Функция увеличивает счетчик количества товаров рядом с иконкой корзины.
-//  */
-// function increaseProductsCount() {
-// 	basketCounterEl.textContent++;
-// }
-//
-// /**
-//  * Функция увеличивает количество товаров в строке в корзине.
-//  * @param {number} productId
-//  */
-// function increaseProductCount(productId) {
-// 	const productCountEl = document.querySelector(`.product-count[data-productId="${productId}"]`);
-// 	productCountEl.textContent++;
-// }
-//
-// /**
-//  * Функция пересчитывает стоимость товара умноженное на количество товара
-//  * в корзине.
-//  * @param {number} productId
-//  */
-// function recalculateSumForProduct(productId) {
-// 	const productTotalRowEl = document.querySelector(`.product-totalRow[data-productId="${productId}"]`);
-// 	let totalPriceForRow = (basket[productId] * products[productId].price).toFixed(2);
-// 	productTotalRowEl.textContent = totalPriceForRow;
-// }
-//
-// /**
-//  * Функция пересчитывает общую стоимость корзины и выводит это значение на страницу.
-//  */
-// function calculateAndRenderTotalBasketSum() {
-// 	let totalSum = 0;
-// 	for (let productId in basket) {
-// 		totalSum += basket[productId] * products[productId].price;
-// 	}
-// 	basketTotalValueEl.textContent = totalSum.toFixed(2);
-// }
-
-// /**
-//  * Эта функция срабатывает когда добавляют новый товар в корзину.
-//  * @param {number} productId
-//  */
-// function addProductIntoBasket(productId) {
-// 	increaseProductsCount();
-// 	addProductToObject(productId);
-// 	renderProductInBasket(productId);
-// 	calculateAndRenderTotalBasketSum();
-// }
